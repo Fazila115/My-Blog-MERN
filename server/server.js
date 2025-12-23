34a8923b8f716229c7eb318ee15751720a7d1d67
Fazila115 <fazilasohail33@gmail.com>
2025-12-23 16:33:46 +0500

    configure server.js

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+dotenv.config();
+connectDB();
+
+app.use(express.json());
+app.use(cors({origin: 'http://localhost:5173', credentials: true}));
+
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`))
