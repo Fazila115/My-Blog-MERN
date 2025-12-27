b26cad07d9011e4103dadd52863d2d47b326f353
Fazila115 <fazilasohail33@gmail.com>
2025-12-27 14:38:30 +0500

    create post schema

---
+++ server/models/post.model.js
@@ -0,0 +1,13 @@
+import { Schema, model } from 'mongoose';
+
+const postSchema = new Schema({
+    title: { type: String, required: true, trim: true },
+    content: { type: String, required: true, },
+    image: { type: String, required: true, },
+    auhtor: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
+    likes: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+}, { timestamps: true });
+
+const Post = model('post', postSchema);
+
+export default Post;
