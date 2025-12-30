3d08dbe43cc30caf83fd83b92aa5b34510428038
Fazila115 <fazilasohail33@gmail.com>
2025-12-30 14:58:41 +0500

    update user routes

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { preSignup, signup, login } from '../controllers/auth.controller.js';
+import { preSignup, signup, login, forgetPassword, resetPassword } from '../controllers/auth.controller.js';
 import upload from '../middleware/upload.js';
 
 const authRouter = express.Router();
@@ -8,5 +8,7 @@ authRouter
     .post('/pre-signup', upload.single('img'), preSignup)
     .get('/signup/:token', signup)
     .post('/login', login)
+    .post('/forget-password', forgetPassword)
+    .post('/reset-password/:token', resetPassword)
 
 export default authRouter;
