79dc35347a7894a78f91dc2974ace00bebdd1009
Fazila115 <fazilasohail33@gmail.com>
2025-12-30 12:10:46 +0500

    setup: crypto

---
+++ server/helper/generateToken.js
@@ -0,0 +1,10 @@
+import crypto from 'crypto';
+
+const generateEmailtoken = () => {
+const token = crypto.randomBytes(32).toSorted('hex');
+const hashedToken = crypto.createHash('sha256').update(token).digest('hex');
+
+return {token, hashedToken};
+};
+
+export default generateEmailtoken;
