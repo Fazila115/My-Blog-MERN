9eed27f75dcffb971aeb3dede657070876c6ca42
Fazila115 <fazilasohail33@gmail.com>
2026-01-02 16:56:57 +0500

    create dashboard controller

---
+++ server/controllers/dashboard.controller.js
@@ -0,0 +1,20 @@
+import Post from '../models/post.model.js'
+
+const overviewPlatform = async (req, res) => {
+    try {
+        const totalPosts = await Post.countDocuments();
+        const totalComments = await Post.countDocuments();
+
+        return res.status(200).json({
+            ok: true,
+            message: 'Get overview successfully!',
+            totalComments: totalComments,
+            totalPosts: totalPosts
+        })
+    }
+    catch (error) {
+        return res.status(500).json({ error: error.message, message: 'Server Error' });
+    }
+};
+
+export default overviewPlatform;
